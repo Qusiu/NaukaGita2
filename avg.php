906019576a51fa2526a8747cf717ff7444883c2c
Hubert <hubcio-98@wp.pl>
2024-04-15 19:33:22 +0200

    Funkcja srednia (wazona)

---
+++ avg.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+	// pamiętaj o zapisaniu pliku (Crtl+S)
+
+	
+
+	function srednia(int a, int b)
+
+	{
+
+		return (a+b)/2;
+
+	}
+
+	
+
+	function wazona_srednia(int a, int b)
+
+	{
+
+		return (3*a+7*b)/10;
+
+	}
+
+?>
